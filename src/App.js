cee60c96a7b20ab467ed8f1226e359f451265c01
Tiago Azevedo Borges <tabgyn@gmail.com>
2019-05-26 16:19:29 -0300

    add global style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 
+import GlobalStyle from "./styles/global";
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
